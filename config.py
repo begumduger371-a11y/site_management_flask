56f47f2b400d15ec6bf28f62ef336745c7e440a7
Yonetici Ahmet <admin@site.com>
2026-05-25 22:46:21 +0300

    feat: projenin Application Factory temel mimarisi ve iskeleti kuruldu

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'cok-gizli-bir-key-1234'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
