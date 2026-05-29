f050d6e726139d16d16564e05f91a0f9f92d62db
begumduger371-a11y <begumduger371@gmail.com>
2026-05-29 22:01:01 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'cok-gizli-bir-key-1234'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False 
